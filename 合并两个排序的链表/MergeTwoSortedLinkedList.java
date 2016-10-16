4a60a2bc29a0aeee13237837a994c5a21dd6ce3a
lileialg <lilei_jordan@163.com>
2016-10-16 10:59:33 +0800

    Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists. 时间复杂度为o(min(m,n)) 空间复杂度为1

---
+++ 合并两个排序的链表/MergeTwoSortedLinkedList.java
@@ -0,0 +1,47 @@
+package threads;
+
+public class MergeTwoSortedLinkedList {
+	
+	static class LinkedNode{
+		int val;
+		
+		LinkedNode next;
+		
+		public LinkedNode(int val){
+			this.val = val;
+		}
+	}
+	
+	public static LinkedNode mergeTwoSortedLinkedList(LinkedNode p1,LinkedNode p2){
+		LinkedNode p = new LinkedNode(-1);
+		
+		while(p1 != null && p2 != null){
+			if (p1.val >= p2.val){
+				p.next = p1;
+				p1 = p1.next;
+			}else{
+				p.next = p2;
+				p2 = p2.next;
+			}
+		}
+		
+		while(p1 != null){
+			p.next = p1;
+			p1 = p1.next;
+		}
+		
+		while(p2 != null){
+			p.next = p2;
+			p2 = p2.next;
+		}
+		
+		return p.next;
+	}
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
